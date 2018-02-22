43d646bae2857641b73afe4c61b0ba3ca9f3a3c8
lemuelkumarga <lemuel@LeMac.local>
2018-02-23 06:02:27 +0800

    Update with new knit function

---
+++ knit.R
@@ -0,0 +1,33 @@
+
+knitter <- function(inputFile, encoding) {
+  
+  # First collect all the csss that are available
+  css = c('shared/css/defaults.css');
+  if (file.exists('../../../shared/css/definitions.css')) { css = c(css, css,'../../../shared/css/definitions.css'); } 
+  if (file.exists('../../../shared/css/general.css')) { css = c(css, css,'../../../shared/css/general.css'); } 
+  css = c(css, 'shared/css/Rmd.css'); 
+  
+  # html output
+  html_output <- rmarkdown::html_document(css=css, 
+                                          code_folding='hide', 
+                                          self_contained=TRUE, 
+                                          theme='cosmo', 
+                                          toc=TRUE, 
+                                          toc_depth=4)
+  html_file <- file.path(dirname(inputFile), 'index.html')
+  rmarkdown::render(inputFile, 
+                    output_format=html_output, 
+                    encoding = encoding, 
+                    output_file = html_file); 
+  
+  # md output
+  md_output <- rmarkdown::md_document()  
+  md_file <- file.path(dirname(inputFile), 'README.md')
+  rmarkdown::render(inputFile, 
+                    output_format=md_output, 
+                    encoding = encoding, 
+                    output_file = md_file); 
+  
+  system('rm -rf README_files');
+}
+ 
