2f14730d49a2b7b6adf6f457ec9d6b60cdb5d938
Lemuel <lemuelkumarga@gmail.com>
2018-06-29 16:52:15 +0800

    Add feature snapshot for classification problems

--- helper.R
+++ helper.R
@@ -1,50 +1,4 @@
 
-## ---- data-overview
-
-# Provides high level information about a particular data frame
-# @input data: the data frame
-# @input null_fn: a function that takes in a value (like those in the cells)
-# and outputs true if value corresponds to the equivalent null value, false
-# otherwise. For example, we may want to consider 0 as nulls on some scenarios
-# but not on others.
-# @output A data frame showing
-# - Name of the Columns
-# - Type of Variables in Each Column
-# - Some Non-Null Examples of Each Column
-# - % of Values that are Non-Null in Each Column (% Filled)
-data_overview <- function(data,
-                          null_fn = function(cname) { paste0(cname," == '' | is.na(",cname,")")}) {
-                  
-                  cols_summary <- data.frame(ColumnNames = colnames(data))
-                  cols_summary$Type <- lapply(data, class) %>%
-                    toupper()
-                  cols_summary$Examples <- lapply(cols_summary$ColumnNames,
-                                                  function(cname) {
-                                                    data %>%
-                                                      filter_(paste0("!(",null_fn(cname),")")) %>%
-                                                      `[[`(cname) %>%
-                                                      unique() -> filtered_set
-                                                    filtered_set[1:min(5, length(filtered_set))] %>%
-                                                      paste(collapse=' // ')
-                                                  })
-                  cols_summary$EmptyValues <- lapply(cols_summary$ColumnNames,
-                                                     function(cname) {
-                                                       data %>%
-                                                         filter_(null_fn(cname)) %>%
-                                                         nrow()
-                                                     })
-                  cols_summary$PctFilled <- lapply(cols_summary$EmptyValues,
-                                                   function(x) {
-                                                     ((nrow(data) - x) / nrow(data)) %>%
-                                                       `*`(100) %>% floor() %>%
-                                                       paste0("%")
-                                                   })
-                  
-                  select(cols_summary, ColumnNames, Type, Examples, PctFilled)
-}
-
-## ---- end-of-data-overview
-
 ## ---- cache
 
 # After running the function, save the output into a cache file
@@ -92,5 +46,240 @@ tictoc <- function(tic, toc, tic_on_toc = FALSE) {
   tt_output
 }
 
-
 ## ---- end-of-tictoc
+
+## ---- data-overview
+
+# Provides high level information about a particular data frame
+# @input data: the data frame
+# @input null_fn: a function that takes in a value (like those in the cells)
+# and outputs true if value corresponds to the equivalent null value, false
+# otherwise. For example, we may want to consider 0 as nulls on some scenarios
+# but not on others.
+# @output A data frame showing
+# - Name of the Columns
+# - Type of Variables in Each Column
+# - Some Non-Null Examples of Each Column
+# - % of Values that are Non-Null in Each Column (% Filled)
+data_overview <- function(data,
+                          null_fn = function(cname) { paste0(cname," == '' | is.na(",cname,")")}) {
+  
+  cols_summary <- data.frame(ColumnNames = colnames(data))
+  cols_summary$Type <- lapply(data, class) %>%
+    toupper()
+  cols_summary$Examples <- lapply(cols_summary$ColumnNames,
+                                  function(cname) {
+                                    data %>%
+                                      filter_(paste0("!(",null_fn(cname),")")) %>%
+                                      `[[`(cname) %>%
+                                      unique() -> filtered_set
+                                    filtered_set[1:min(5, length(filtered_set))] %>%
+                                      paste(collapse=' // ')
+                                  })
+  cols_summary$EmptyValues <- lapply(cols_summary$ColumnNames,
+                                     function(cname) {
+                                       data %>%
+                                         filter_(null_fn(cname)) %>%
+                                         nrow()
+                                     })
+  cols_summary$PctFilled <- lapply(cols_summary$EmptyValues,
+                                   function(x) {
+                                     ((nrow(data) - x) / nrow(data)) %>%
+                                       `*`(100) %>% floor() %>%
+                                       paste0("%")
+                                   })
+  
+  select(cols_summary, ColumnNames, Type, Examples, PctFilled)
+}
+
+## ---- end-of-data-overview
+
+## ---- plot-overview
+
+# Load defaults.R and other necessary packages
+source(paste0(dirname(sys.frame(1)$ofile),"/defaults.R"))
+load_or_install.packages(c("ggplot2","dplyr","rms","GGally"))
+
+# Provides a relationship snapshot amongst features and the class
+# @input dataset: the dataset
+# @input response: the column name containing the observed class
+# @output A plot showing
+# - Diagonal: The density of varibale for each class
+# - Lower Triangle: A contour density plot
+# - Upper Triangle: Correlation plot
+class_snapshot <- function(dataset, response) {
+  
+  # Rank Each Feature Based On Their R^2 using logit
+  r2 <- sapply(setdiff(colnames(dataset),response), function(f) {
+    eqn <- as.formula(paste(response,"~",f))
+    model <- lrm(eqn, data = dataset)
+    return(model$stats[["R2"]])
+  }) %>% sort(decreasing = TRUE)
+  
+  # Create Default Settings Across Plots
+  r_levels <- levels(dataset[,response])
+  # Colors
+  if (length(r_levels) > length(get_color())) { 
+    r_cols <- get_color("palette")(length(r_levels)) 
+  } else { 
+    r_cols <- get_color()[1:length(r_levels)] 
+  }
+  names(r_cols) <- r_levels
+  
+  # Legends
+  legend_p <- (ggplot(dataset, aes_string(x=response, fill=response)) + 
+                 geom_histogram(stat="count") +
+                 theme_lk() + 
+                 theme(legend.position = "top",
+                       legend.justification = c(0.5,0)) +
+                 scale_fill_manual(name=response, values=r_cols,
+                                   guide=guide_legend())) %>%
+    grab_legend()
+  
+  # ############################# 
+  # DIAGONAL PLOTS ##############
+  diag_p <- function(data, mapping, ...) {
+    
+    # Get Feature and Response Variable
+    feature <- quo_name(mapping$x)
+    p_output <- ggplot(data)
+    
+    f_factor <- is.factor(data[,feature])
+    # If feature is discrete, compare conditional distributions on feature
+    if (f_factor) {
+      p_output <- p_output + 
+        geom_histogram(aes_string(x=feature, fill=response, alpha="..count.."), 
+                       stat="count",
+                       colour = NA, position="fill") + 
+        scale_x_discrete(expand=c(0,0))
+      # If feature is continuous, plot a density plot
+    } else {
+      p_output <- p_output + 
+        geom_density(aes_string(x=feature, fill=response), 
+                     alpha=0.5, colour = NA) + 
+        scale_x_continuous(expand=c(0,0))
+    }
+    
+    # Output plot
+    p_output +
+      geom_text(data = data.frame(x=Inf, y=Inf, label = scales::percent(r2[[feature]])),
+                aes(x=x, y=y, label=label),
+                family=def_font, hjust=1.3, vjust=2.0) + 
+      scale_y_continuous(expand=c(0,0)) +
+      scale_fill_manual(values=r_cols)
+  }
+  
+  # ############################# 
+  # LOWER PLOTS #################
+  lower_p <- function(data, mapping, ...) {
+    
+    # Get Feature1 and Feature2 Variable
+    x <- quo_name(mapping$x)
+    y <- quo_name(mapping$y)
+    
+    # Determine if x and y is discrete or continuous
+    x_factor <- is.factor(data[,x])
+    y_factor <- is.factor(data[,y])
+    
+    # Case 1: Both x and y are continuous
+    if (!x_factor & !y_factor) {
+      p_out <- ggplot(data) +
+        geom_density_2d(aes_string(x=x, y=y,  colour=response), alpha=0.3) +
+        scale_x_continuous(expand=c(0,0)) + 
+        scale_y_continuous(expand=c(0,0)) + 
+        scale_color_manual(values=r_cols)
+      # Case 2: x is continuous, y is discrete
+    } else if (!x_factor & y_factor) {
+      p_out <- ggplot(data) +
+        geom_density(aes_string(x=x, fill=response),
+                     colour=NA, alpha=0.3) + 
+        scale_x_continuous(expand=c(0,0)) + 
+        scale_y_continuous(expand=c(0,0)) +
+        scale_fill_manual(values=r_cols) +
+        facet_wrap(as.formula(paste0("~",y)), ncol=1) + 
+        theme(strip.background = element_blank(),
+              strip.text.x = element_blank())
+      
+    } else {
+      p_out <- ggplot(data)
+      # Case 3: x is discrete and y is continuous
+      if (!y_factor) { 
+        p_out <- p_out + 
+          geom_histogram(aes_string(x=y, fill=response, alpha="..count.."),
+                         position="fill", bins = 10) + 
+          scale_x_continuous(expand=c(0,0)) 
+        # Case 4: x is discrete and y is discrete
+      } else { 
+        p_out <- p_out + 
+          geom_histogram(aes_string(x=y, fill=response, alpha="..count.."),
+                         stat="count",
+                         position="fill") + 
+          scale_x_discrete(expand=c(0,0))
+      }
+      p_out <- p_out +
+        scale_y_continuous(expand=c(0,0)) +
+        scale_fill_manual(values=r_cols) + 
+        coord_flip() +
+        facet_wrap(as.formula(paste0("~",x))) + 
+        theme(strip.background = element_blank(),
+              strip.text.x = element_blank())
+    }
+    
+  }
+  
+  # ############################# 
+  # UPPER PLOTS #################
+  upper_p <- function(data, mapping, ...) {
+    
+    # Get Feature1 and Feature2 Variable
+    x <- quo_name(mapping$x)
+    y <- quo_name(mapping$y)
+    
+    # Determine if y is discrete or continuous
+    y_factor <- is.factor(data[,y])
+    eqn <- as.formula(paste(y,"~",x))
+    relation <- 0
+    # if discrete, run logistic regression
+    if (y_factor) {
+      model <- lrm(eqn, data)
+      relation <- round(sqrt(model$stats[["R2"]]),2)
+      # else if continuous, run linear regression
+    } else {
+      model <- lm(eqn, data)
+      relation <- round(sqrt(summary(model)$r.squared),2)
+    }
+    
+    # Output plot
+    ggplot(data=data.frame(1)) + 
+      theme_void() + 
+      scale_x_continuous(limits=c(0,1)) + 
+      scale_y_continuous(limits=c(0,1)) +
+      geom_label(x=0.5, y=0.5, 
+                 label=scales::percent(relation), 
+                 family=def_font, color=bg_color, fill=fade_color(txt_color,0.2), 
+                 size=relation*10)
+  }
+  
+  # Output Plot
+  ggpairs(dataset, aes_string(colour=response),columns=names(r2),
+          lower=list(continuous=GGally::wrap(lower_p),
+                     combo=GGally::wrap(lower_p),
+                     discrete=GGally::wrap(lower_p)),
+          diag=list(continuous=GGally::wrap(diag_p),
+                    discrete=GGally::wrap(diag_p)),
+          upper=list(continuous=GGally::wrap(upper_p),
+                     combo=GGally::wrap(upper_p),
+                     discrete=GGally::wrap(upper_p)),
+          axisLabels="none",
+          switch="y",
+          legend=legend_p) + 
+    theme_lk() + 
+    theme(strip.background = element_rect(fill=NA),
+          strip.text = element_text(colour=ltxt_color, size=12),
+          panel.spacing = unit(3,"pt"),
+          panel.border = element_rect(color = fade_color(txt_color,0.3), fill = NA, size = 0.1),
+          axis.title.x = element_text(margin=margin(15,0,0,0))) + 
+    xlab(bquote("LT = 2D Density View     D = Density (With "*R^2*")    UT = Correlation"))
+}
+
+## ---- end-of-plot-overview
